96c8adad24f66b9fbc8010cca6b14b3fcdd48525
wandji <wbertrand10@gmail.com>
2021-03-18 20:09:45 +0100

    Update user resource and make create action for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,27 @@
 class UsersController < ApplicationController
   def index; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path,
+      notice: "Welcome #{@user.name}\n Your Account is Ready"
+    else
+      render :new,
+      alert: "#{@user.name} is already taken\n Please input a name"
+    end
+  end
+
+  def show
+    @user = User.find_by()
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
-  resources :users, only: [:index, :destroy, :create ]
+  resources :users, only: [:index, :create, :new, :show]
 end
