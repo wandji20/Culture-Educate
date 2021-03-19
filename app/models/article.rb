a911066e15443e05b39f58e437f84c8a1317d027
wandji <wbertrand10@gmail.com>
2021-03-19 14:59:20 +0100

    Add associations for user, article, and category

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
-  belongs_to :author
+  validates :title, presence: true, length: { in: (3..15) }
+  validates :body, presence: true,length: { in: (15..200) }
+  belongs_to :author, class_name: 'User'
+  has_and_belongs_to_many :categories
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { in: (3..10) }
+  has_many :authored_articles,class_name: 'Article', foreign_key: :author_id, dependent: :destroy
 end
