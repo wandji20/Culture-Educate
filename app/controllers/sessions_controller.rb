5626fa42d9695673c1bc540800301e7505c395f8
wandji <wbertrand10@gmail.com>
2021-03-19 09:02:08 +0100

    Add create method for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    @user = User.find_by(:name params[:name])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to root_path,
+      notice: "Welcome @user.name\n You have successfully signed in"
+    else
+      render :new,
+      alert: 'Please sign up or sign in to continue'
+    end
+    
+  end
+
+  def destroy
+
+  end
 end
