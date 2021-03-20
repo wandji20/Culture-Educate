f6c11f523948c43a1cd7709762b36b945b589448
wandji <wbertrand10@gmail.com>
2021-03-20 10:00:06 +0100

    Add route for article and update nav link for new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
   def new
-    @article = current_user.articles.new
+    @article = current_user.authored_articles.new
   end
 
   def create
-    @article = currrent_user.articles.build(article_params[:article])
+    @article = currrent_user.authored_articles.build(article_params[:article])
     if @aricle.save
       redirect_to root_path,
       notice: 'Article successfully created'
@@ -12,5 +12,10 @@ class ArticlesController < ApplicationController
       render :new,
       alert: 'Please check the form and re-submit'
     end
+  end
+
+  private
+  def article_params
+    params.permit(:article).require(:article, :category_attributes)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :create, :new, :show]
   resources :sessions, only: [:new, :create, :destroy]
-  
+  resources :articles, only: [:new, :create, :destroy, :edit]
   # get 'logout', to: 'sessions#destroy', as: 'logout'
   # get 'new_login', to: 'sessions#new', as: 'new_login'
   # post 'login', to: 'sessions#create', as: 'login'
