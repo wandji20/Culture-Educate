de630adbc41445ac7639a80676e1f4f1fb9dc351
wandji <wbertrand10@gmail.com>
2021-03-21 17:54:59 +0100

    Change style display fir articles to use css grid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_21_121505) do
+ActiveRecord::Schema.define(version: 2021_03_21_151529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,17 @@ ActiveRecord::Schema.define(version: 2021_03_21_121505) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.bigint "article_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_votes_on_article_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
   add_foreign_key "articles_categories", "articles"
   add_foreign_key "articles_categories", "categories"
+  add_foreign_key "votes", "articles"
+  add_foreign_key "votes", "users"
 end
