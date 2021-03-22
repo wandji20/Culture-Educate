34efa3ddda2c5b9f34c67177854faf7f38687235
wandji <wbertrand10@gmail.com>
2021-03-22 10:35:07 +0100

    Add upvote_or_down_vote helper method to display either upvote or downvote

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
       alert
     end
   end
+
+  def upvote_or_downvote(article)
+    @vote = Vote.find_by(article: article, user: current_user)
+    unless @vote
+      link_to 'Upvote', votes_path(article_id: article.id, category_id: params[:id]), method: :post
+    else
+      link_to 'Downvote', vote_path(article_id: article.id, category_id: params[:id]), method: :delete
+    end
+  end
 end
