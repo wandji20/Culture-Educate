2493fac70f72c5874be1156504aaf3b2f11685fe
wandji <wbertrand10@gmail.com>
2021-03-22 09:36:42 +0100

    Add create and destroy action for votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,25 @@
 class VotesController < ApplicationController
 
   def create
-    
+    @vote = Vote.new(article_id: params[:article_id], user_id: current_user.id)
+    @category = Category.find(params[:category_id])
+    if @vote.save
+      redirect_to @category,
+      notice: 'Thanks for your vote'
+    else
+      redirect_to root_path,
+      alert: 'Sonething went wrong\n Please vote again'
   end
 
   def destroy
+    @vote = Vote.find(params[:id])
+    @category = Category.find(params[:category_id])
+    if @vote.destroy
+      redirect_to @category,
+      notice: 'You can vote again'
+    else
+      redirect_to @category,
+      alert: 'Something went wrong. Try down voting again'
+    end
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,7 @@ class Article < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :categories
   has_many :votes, dependent: :destroy
-  #has_one_attached :image
+  
   include ImageUploader::Attachment(:image)
 
   # accepts_nested_attributes_for :categories 
