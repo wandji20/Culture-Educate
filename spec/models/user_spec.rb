c07b4d684a46d3b7d376b6adb37fdbd01a4bece1
wandji <wbertrand10@gmail.com>
2021-03-23 10:16:28 +0100

    Add unit test for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validation' do
+    it{should validate_uniqueness_of(:name)}
+    it{should validate_presence_of(:name)}
+    it{should validate_length_of(:name).
+      is_at_least(3).is_at_most(10)}
+  end
+
+  context 'Associations' do
+    it { should have_many(:authored_articles) }
+    it { should have_many(:votes) }
+  end
 end
