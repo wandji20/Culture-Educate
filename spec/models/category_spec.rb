198fc7fdcf123fd73cb2664b4c13becd54b8f42d
wandji <wbertrand10@gmail.com>
2021-03-23 09:40:45 +0100

    Add unit test for category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :categories
+
+  context 'validation' do 
+    it 'validates name presence' do
+      should validate_presence_of(:name)
+    end
+
+    it 'validates name uniqueness' do
+      should validate_uniqueness_of(:name)
+    end
+ 
+  end
 end
