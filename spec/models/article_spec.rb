fd0f80ce5d2270489e0e33388b0307db18dc7d2f
wandji <wbertrand10@gmail.com>
2021-03-23 09:03:43 +0100

    Add association test for articlescategories

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  fixtures :users, :articles
+
+  context 'title validation' do
+    it 'saves article with valid attributes' do
+      article = articles(:article1)
+      expect(article.valid?).to be true
+    end
+    it 'validates title presence' do
+      article = articles(:article4)
+      expect(article.valid?).to be false
+    end
+  end
+
+  context 'Body validation' do
+    it 'validates body length' do
+      article = articles(:article2)
+      expect(article.valid?).to be false
+    end
+  end
+
+  context 'associations for article' do
+    it { should belong_to(:author) }
+    it { should have_db_column(:body) }
+    it { should have_db_column(:image_data )}
+  end
+
 end
--- spec/models/articles_category_spec.rb
+++ spec/models/articles_category_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  context 'associations for articlescategories' do
+    it { should belong_to(:article) }
+    it { should belong_to(:category) }
+    it { should have_db_column(:article_id) }
+    it { should have_db_column(:category_id )}
+  end
+
 end
+
