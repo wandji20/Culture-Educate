f7eee1b0b221fa5bc380e1e02606462adf38753a
wandji <wbertrand10@gmail.com>
2021-03-24 17:49:15 +0100

    Changes for heroku

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -6,12 +6,11 @@ api_key:    ENV['CLOUD_API_KEY'],
 api_secret: ENV['CLOUD_API_SECRET'],
 )
 Shrine.storages = {
-cache: Shrine::Storage::Cloudinary.new(prefix: "culture-educate/cache"), # for direct uploads
-store: Shrine::Storage::Cloudinary.new(prefix: "culture-educate"),
+cache: Shrine::Storage::Cloudinary.new(prefix: "cache"), # for direct uploads
+store: Shrine::Storage::Cloudinary.new(prefix: "rails_uploads"),
 }
 Shrine.plugin :activerecord           # loads Active Record integration
 Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
 Shrine.plugin :restore_cached_data    # extracts metadata for assigned cached files
 Shrine.plugin :validation_helpers
 Shrine.plugin :validation
-‍
